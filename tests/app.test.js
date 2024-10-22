84d89d0eda9b1b35147eb99055c6de6d05a8737f
Ximena Urbieta <xurbieta@gmail.com>
2024-10-22 16:52:26 +0200

    second change deploy-gitactions

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('API Tests', () => {
-    it('should return Welcome to the new developers from Fly.io on GET /', async () => {
+    it('should return Welcome to the new developers from Fly.io second time on GET /', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
-        expect(response.body.message).toBe('Welcome to the new developers from Fly.io');
+        expect(response.body.message).toBe('Welcome to the new developers from Fly.io second time');
     });
 
     it('should echo back the posted data on POST /echo', async () => {
