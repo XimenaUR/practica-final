f7737d5845d5db0f25b8816a4e4f7f30c9958fa2
Ximena Urbieta <xurbieta@gmail.com>
2024-10-29 11:02:15 +0100

    update practica final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 // Ruta básica
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the new developers from Fly.io third time' });
+    res.status(200).json({ message: 'Welcome to the new developers from Fly.io practica final!' });
 });
 
 // Ruta de ejemplo para pruebas
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('API Tests', () => {
-    it('should return Welcome to the new developers from Fly.io third time on GET /', async () => {
+    it('should return Welcome to the new developers from Fly.io practica final! on GET /', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
-        expect(response.body.message).toBe('Welcome to the new developers from Fly.io third time');
+        expect(response.body.message).toBe('Welcome to the new developers from Fly.io practica final!');
     });
 
     it('should echo back the posted data on POST /echo', async () => {
